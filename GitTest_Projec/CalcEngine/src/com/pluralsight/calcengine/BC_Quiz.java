817ddc8052f3fd4c825c57e3226d96c4faed73d2
aryan897singh <aryan897work@gmail.com>
2025-07-11 00:30:52 -0700

    Version 1.0

---
+++ GitTest_Projec/CalcEngine/src/com/pluralsight/calcengine/BC_Quiz.java
@@ -0,0 +1,26 @@
+package com.pluralsight.calcengine;
+
+public class BC_Quiz {
+    public static void main(String[] args) {
+        diamond();
+        diamond();
+        half_diamond();
+
+    }
+
+    static void diamond(){
+        System.out.println("             /\\                   " );
+        System.out.println("            /  \\                     ");
+        System.out.println("           /    \\                  ");
+        System.out.println("           \\    /                 ");
+        System.out.println("            \\  /                  ");
+        System.out.println("             \\/                    ");
+    }
+
+    static void half_diamond(){
+        System.out.println("             /\\                   " );
+        System.out.println("            /  \\                     ");
+        System.out.println("           /    \\                  ");
+
+    }
+}
